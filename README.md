e66d2fe94fb1179510cb4063598065625e1fa224
5undel <max.sundel@Choice.se>
2021-09-24 21:23:18 +0000

    Move document folder out of assets folder

--- README.md
+++ README.md
@@ -10,15 +10,15 @@ __Features__
 - scoreborad
     - The scoreboard is an easy way to see the scores for the user and computer. The user is on the left side and the computer on the right.
     - The scoreboard has a clean look at the top center whit a good contrast for the text color and the background color. 
-    ![image](assets/document/scorebord.png)
+    ![image](document/scorebord.png)
 
 - Game option
     - The game options are easy to choose from. And when hover over the size change to smaller and the border gets orange to make it easeyer fot the user to see witch move they choos.
     - The winner is shown over the game options with a easy to read text that shows if the player is the winner or the computer, or if it is a draw.
-     ![image](assets/document/r-p-s.png)
+     ![image](document/r-p-s.png)
 
-__footer__
-
+- Footer
  - In the footer is a link that the user can click to open a new tab to the creator github.
+ ![image](document/link.png)
  
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,127 +1,107 @@
 /* jshint esversion: 8 */
-/**
- * cashing the Dom for future use
- */
-let userScore = 0;
-let computerScore = 0;
-const userScoreSpan =  document.getElementById("user-score");
-const computerScoreSpan =  document.getElementById("computer-score");
-const scoreDiv = document.querySelector(".score");
-const resultDiv = document.querySelector(".result > p");
-const rockDiv = document.getElementById("rock");
-const paperDiv = document.getElementById("paper");
-const scissorDiv = document.getElementById("scissor");
-const resetSection = document.getElementsByTagName("reset")
 
-/**
- * 
- * computer random choice code
- */
-function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissor'];
-    const randomNum = Math.floor(Math.random() * 3);
-    return choices [randomNum];
-}
-
-/**
- * change the first latter to uppercase
- */
-function word (letter) {
-    if (letter === "rock") return "Rock";
-    if (letter === "paper") return "Paper";
-    return "Scissor";
-}
-
-/**
- * win function 
- */
-function win(user, computer){
-    userScore++;
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML =computerScore;
-    resultDiv.innerHTML = word(user) + " beats " + word(computer) +". You Win!"
-} 
-
-/**
- * lose function 
- */
-function lose(user, computer){
-    computerScore++;
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML = computerScore;
-    resultDiv.innerHTML = word(user) + " loses to " + word(computer) +". You Lost..."
-} 
-
-/**
- * draw function 
- */
-function draw(user, computer){
-    userScoreSpan.innerHTML = userScore;
-    computerScoreSpan.innerHTML =computerScore;
-    resultDiv.innerHTML = word(user) + " friends " + word(computer) +". It´s A Draw"
-} 
-
-/**
- * game function  
- */
-function game(userChoice) {
-    const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case "rockscissor":
-        case "paperrock":
-        case "scissorpaper":
-            win(userChoice, computerChoice);
-            break;
-        case "rockpaper":
-        case "paperscissor":
-        case "scissorrock":
-            lose(userChoice, computerChoice);
-            break;
-        case "rockrock":
-        case "paperpaper":
-        case "scissorscissor":
-            draw(userChoice, computerChoice);
-            break;
-    }
-}
-
-
-/**
- * open the popup
-*/
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
-/**
- * reset game
- */
-function reset() {
-    userScore = 0;
-    computerScore = 0;
+//cashing the Dom for future use
+ let userScore = 0;
+ let computerScore = 0;
+ const userScoreSpan =  document.getElementById("user-score");
+ const computerScoreSpan =  document.getElementById("computer-score");
+ const resultDiv = document.querySelector(".result > p");
+ const rockDiv = document.getElementById("rock");
+ const paperDiv = document.getElementById("paper");
+ const scissorDiv = document.getElementById("scissor");
+ const resetSection = document.getElementsByTagName("reset");
+ const popupSection = document.getElementsByClassName("popup")
+ 
+ //computer random choice code
+ function getComputerChoice() {
+     const choices = ['rock', 'paper', 'scissor'];
+     const randomNum = Math.floor(Math.random() * 3);
+     return choices [randomNum];
+ }
+ 
+ // change the first latter to uppercase
+ function word (letter) {
+     if (letter === "rock") return "Rock";
+     if (letter === "paper") return "Paper";
+     return "Scissor";
+ }
+ 
+ //win function 
+ function win(user, computer){
+     userScore++;
+     userScoreSpan.innerHTML = userScore;
+     computerScoreSpan.innerHTML =computerScore;
+     resultDiv.innerHTML = word(user) + " beats " + word(computer) +". You Win!";
+ } 
+ 
+ //lose function 
+ function lose(user, computer){
+     computerScore++;
+     userScoreSpan.innerHTML = userScore;
+     computerScoreSpan.innerHTML = computerScore;
+     resultDiv.innerHTML = word(user) + " loses to " + word(computer) +". You Lost...";
+ } 
+ 
+ //draw function 
+ function draw(user, computer){
+     userScoreSpan.innerHTML = userScore;
+     computerScoreSpan.innerHTML =computerScore;
+     resultDiv.innerHTML = word(user) + " friends " + word(computer) +". It´s A Draw";
+ } 
+ 
+ //game function
+ function game(userChoice) {
+     const computerChoice = getComputerChoice();
+     switch (userChoice + computerChoice) {
+         case "rockscissor":
+         case "paperrock":
+         case "scissorpaper":
+             win(userChoice, computerChoice);
+             break;
+         case "rockpaper":
+         case "paperscissor":
+         case "scissorrock":
+             lose(userChoice, computerChoice);
+             break;
+         case "rockrock":
+         case "paperpaper":
+         case "scissorscissor":
+             draw(userChoice, computerChoice);
+             break;
+     }
+ }
+ 
+ 
+ //open the popup
+ function pupupSection() {
+     var popup = document.getElementById("myPopup");
+     popup.classList.toggle("show");
+     
+   }
+ 
+   //reset game - not working jet
+ function reset() {
+     userScore = 0;
+     computerScore = 0;
+     
+     
+ }
+ 
+ main();
+ 
+ function main() {
+ 
+     rockDiv.addEventListener('click', function(){
+     game("rock");
+     });
+ 
+     paperDiv.addEventListener('click', function(){
+         game("paper");
+     });
+ 
+     scissorDiv.addEventListener('click', function(){
+         game("scissor");
+     });
+ 
     
-    scoreDiv.innerHTML = '';
-
-    rockDiv.removeAttribute("disabled");
-    paperDiv.removeAttribute("disabled");
-    scissorDiv.removeAttribute("disabled");
-}
-
-main();
-
-function main() {
-
-    rockDiv.addEventListener('click', function(){
-    game("rock")
-    })
-
-    paperDiv.addEventListener('click', function(){
-        game("paper")
-    })
-
-    scissorDiv.addEventListener('click', function(){
-        game("scissor")
-    })
-
-   
-}
+ }
